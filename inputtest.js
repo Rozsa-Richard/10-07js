8e2e1e0e76a127f7abddb7c41054649dd01bde87
Rikky <rozsarichardzon@gmail.com>
2024-10-07 11:35:58 +0200

    aha

---
+++ inputtest.js
@@ -0,0 +1,18 @@
+import input from './input.js'
+
+//0
+let txt ="teveMereKereszttizakarmiNemTudom"; 
+//1
+console.log(txt.slice(0,5))
+//2
+console.log(txt.slice(1,9))
+//3
+console.log(txt.slice(4))
+//4
+console.log(txt.slice(4,10))
+//5
+console.log(txt.toUpperCase())
+//6
+//7
+console.log(txt.replace("e","E"))
+//8
